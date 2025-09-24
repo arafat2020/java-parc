fc956bd7045199adddcaffd0897221f74fa2c75a
arafat2020 <arafatmannan2019@gmail.com>
2025-09-24 10:23:45 +0600

    ADD: Selction Sort init

---
+++ src/main/java/com/dsa/SelectionSort.java
@@ -0,0 +1,28 @@
+package com.dsa;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {64, 25, 12, 22, 11};
+        selectionSort(arr);
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
