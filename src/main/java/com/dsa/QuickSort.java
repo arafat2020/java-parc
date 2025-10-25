ccc25217b513e58aab6a26a8fd3d1500206a6e3e
arafat2020 <arafatmannan2019@gmail.com>
2025-10-25 11:11:33 +0600

    ADD: Quick sort wiht hoare method

---
+++ src/main/java/com/dsa/QuickSort.java
@@ -0,0 +1,48 @@
+package com.dsa;
+
+public class QuickSort {
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low - 1;
+        int j = high + 1;
+
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j)
+                return j;
+
+            swap(arr, i, j);
+        }
+    }
+
+    private static void quick_sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quick_sort(arr, low, pivotIndex - 1);
+            quick_sort(arr, pivotIndex + 1, high);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int n = arr.length;
+        quick_sort(arr, 0, n - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
